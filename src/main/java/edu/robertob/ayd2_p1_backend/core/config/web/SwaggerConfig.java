2526cf2aa9d981e6599a7a0177da45ac90d74deb
Roberto Bautista <44622828+rrobertobt@users.noreply.github.com>
2026-03-07 21:10:25 -0600

    feat: enhance Swagger configuration with JWT security scheme support

--- src/main/java/edu/robertob/ayd2_p1_backend/core/config/web/SwaggerConfig.java
+++ src/main/java/edu/robertob/ayd2_p1_backend/core/config/web/SwaggerConfig.java
@@ -1,15 +1,30 @@
 package edu.robertob.ayd2_p1_backend.core.config.web;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
+
+    private static final String SECURITY_SCHEME_NAME = "Bearer Authentication";
+
+    private SecurityScheme createJwtSecurityScheme() {
+        return new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT");
+    }
+
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
+                .components(new Components().addSecuritySchemes(SECURITY_SCHEME_NAME, createJwtSecurityScheme()))
                 .info(new io.swagger.v3.oas.models.info.Info().title("REST API - Project 1-B AyD2")
                         .version("1.0")
                         .description("Documentación de la API para el Proyecto 1 del curso de Análisis y Diseño de Sistemas 2"));
